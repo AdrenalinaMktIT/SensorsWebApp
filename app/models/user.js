e2eddffa1c22b175316fd526ce573288b90bcd8a
sbalazote <sbalazote@gmail.com>
2017-01-31 14:32:48 -0300

    +agrego faltante logica passport para registracion local

--- app/models/user.js
+++ app/models/user.js
@@ -31,7 +31,12 @@ var userSchema = mongoose.Schema({
 
 });
 
-// checking if password is valid using bcrypt
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
 userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.local.password);
 };
