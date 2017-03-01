eb020f13309bad26956cc60a883e5aec92c90c4f
sbalazote <sbalazote@gmail.com>
2017-03-01 11:32:36 -0300

    +desactivo autocomplete en login y signup. +agrego opciones de sesion de express para evitar warnings.

--- server.js
+++ server.js
@@ -47,7 +47,11 @@ app.use(express.static(__dirname + '/public'));
     app.set('view engine', 'ejs'); // set up ejs for templating
 
     // required for passport
-    app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+    app.use(session({
+        secret: 'adrenalinamktsensoreswebappnodejs',
+        resave: false,
+        saveUninitialized: false
+    }));
     app.use(passport.initialize());
     app.use(passport.session()); // persistent login sessions
     app.use(flash()); // use connect-flash for flash messages stored in session
