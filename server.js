fc196007c41dbdc3b51942140da368d59f5fd5f1
sbalazote <sbalazote@gmail.com>
2017-03-10 14:57:15 -0300

    +agrego paginas de error 404.ejs y 500.ejs

--- server.js
+++ server.js
@@ -17,53 +17,76 @@ var favicon = require('serve-favicon');
 var configDB = require('./app/config/database.js');
 
 // configuration ===============================================================
-mongoose.connect(configDB.remoteUrl); // connect to our database
-mongoose.Promise = require('bluebird');
 
 require('./app/config/passport')(passport); // pass passport for configuration
 
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect(configDB.remoteUrl);
+mongoose.Promise = require('bluebird');
+var db = mongoose.connection;
+
+// CONNECTION EVENTS
+// Conexion con Mongo exitosa.
+db.on('connected', function () {
+    console.log('Mongoose abierto en: ' + configDB.remoteUrl);
+});
+
+// Conexion con Mongo fallida.
+db.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// // Conexion con Mongo desconectada.
+db.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
 
 // log every request to the console
-    app.use(morgan('dev'));
+app.use(morgan('dev'));
 
-    // get all data/stuff of the body (POST) parameters
-    // parse application/json
-    app.use(bodyParser.json());
+// get all data/stuff of the body (POST) parameters
+// parse application/json
+app.use(bodyParser.json());
 
-    // parse application/vnd.api+json as json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-    // parse application/x-www-form-urlencoded
-    app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-    app.use(methodOverride('X-HTTP-Method-Override'));
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override'));
 
-    app.use(cookieParser());
+app.use(cookieParser());
 
-    app.set('views', __dirname + '/public/views');
+app.set('views', __dirname + '/public/views');
 
-    app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-    // required for passport
-    app.use(session({
-        secret: 'adrenalinamktsensoreswebappnodejs',
-        resave: false,
-        saveUninitialized: false
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session()); // persistent login sessions
-    app.use(flash()); // use connect-flash for flash messages stored in session
+// required for passport
+app.use(session({
+    secret: 'adrenalinamktsensoreswebappnodejs',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-    app.use(favicon(__dirname + '/public/assets/img/favicon.ico'));
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-//});
+app.use(favicon(__dirname + '/public/assets/img/favicon.ico'));
+
+// Funcion Middleware que es llamada para cada request.
+// En este caso siempre hago un check para ver si la base de datos esta conectada.
+app.use(function (req, res, next) {
+    console.log('Request Time:', Date.now());
+    next();
+});
 
 // APP routes ======================================================================
 require('./app/routes/app.routes')(app, passport); // load our routes and pass in our app and fully configured passport
-// APP routes ======================================================================
+// API routes ======================================================================
 require('./app/routes/alert.routes')(app);
 require('./app/routes/carrier.routes')(app);
 require('./app/routes/client.routes')(app);
@@ -75,6 +98,16 @@ require('./app/routes/sensor.routes')(app);
 require('./app/routes/timezone.routes')(app);
 require('./app/routes/type.routes')(app);
 require('./app/routes/user.routes')(app);
+
+// Manejador error cod. HTTP 404.
+app.use(function(req, res) {
+    res.status(400).render('404.ejs', {title: '404: Recurso no encontrado'});
+});
+
+// Manejador error cod. HTTP 500.
+app.use(function(error, req, res, next) {
+    res.status(500).render('500.ejs', {title:'500: Error interno de servidor.', error: error});
+});
 
 // launch ======================================================================
 app.listen(port);
