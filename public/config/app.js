8d65cb4e83c4f8c4d7dd1682570049cbb51ff71b
sbalazote <sbalazote@gmail.com>
2017-03-22 16:40:10 -0300

    +agrego pantalla de status de sensores con valores hardcodeados y la pongo por default al ingresar al sistema.

--- public/config/app.js
+++ public/config/app.js
@@ -1,11 +1,11 @@
 var app = angular.module('sensorsWebApp', [
-    'alertController', 'deviceController', 'groupController', 'historicalController', 'profileController', 'sensorController', 'userController',
+    'alertController', 'deviceController', 'groupController', 'historicalController', 'profileController', 'sensorController', 'statusController', 'userController',
     'appAlertService', 'alertService', 'carrierService', 'clientService', 'deviceService', 'groupService', 'profileService', 'reportService', 'sensorService', 'typeService', 'timezoneService', 'userService',
     'ui.router', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.pagination', 'ui.grid.selection', 'ui.grid.exporter', 'chart.js', 'ngMessages', 'ui.select', 'ngAnimate', 'ngSanitize']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/devices');
+    $urlRouterProvider.otherwise('/status');
 
     $stateProvider
 
@@ -50,5 +50,10 @@ app.config(function($stateProvider, $urlRouterProvider) {
         url: '/historical',
         templateUrl: '../views/partial-historical.html',
         controller: 'HistoricalCtrl'
+        })
+        .state('status', {
+            url: '/status',
+            templateUrl: '../views/partial-status.html',
+            controller: 'StatusCtrl'
         });
 });
---
+++ public/controllers/status.controller.js
@@ -0,0 +1,6 @@
+angular.module('statusController', [])
+
+// inject the Alert service factory into our controller
+    .controller('StatusCtrl', function (Alerts) {
+
+    });
