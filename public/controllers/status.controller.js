e3c05ff240e42faf04beca4d50e12d34cbfe6bf5
sbalazote <sbalazote@gmail.com>
2017-06-05 16:02:54 -0300

    +cambio dibujo de sensores en pantalla de status de tipo Gauge.

--- public/controllers/status.controller.js
+++ public/controllers/status.controller.js
@@ -2,16 +2,20 @@ angular.module('statusController', [])
     .controller('StatusCtrl', function ($scope, $interval, $uibModal, allStatuses, allGroups, Reports) {
         var vm = this, imei, model, sensor, data, timestamp, status, checkingStatus;
 
+        const intervalDelay = 30000;
+
         vm.sensorList = [];
         vm.sensorListCopy = [];
         vm.filteredByGroupSensorList = [];
         vm.filteredByStatusSensorList = [];
 
+        vm.sensorChartConfig = [];
+
         vm.animationsEnabled = true;
 
         checkingStatus = $interval(function() {
             checkStatus()
-        }, 10000);
+        }, intervalDelay);
 
         vm.groups = allGroups.data.groups;
         vm.groups.unshift({
@@ -106,11 +110,145 @@ angular.module('statusController', [])
             return isOk;
         };
 
+        let drawSensors = function () {
+            _.each(vm.sensorList, function (value, key, list) {
+
+                let sensorNameClass;
+
+                switch (value.status) {
+                    case 'critical':
+                        sensorNameClass = 'bg-danger';
+                        break;
+                    case 'warning':
+                        sensorNameClass = 'bg-warning';
+                        break;
+                    case 'ok':
+                        sensorNameClass = 'bg-success';
+                        break;
+                    default:
+                        sensorNameClass = 'bg-primary';
+                }
+
+                vm.sensorChartConfig[key] = {
+
+                    credits: {
+                        enabled: false
+                    },
+
+                    chart: {
+                        backgroundColor: '#c8c8c8',
+                        type: 'gauge',
+                        plotBackgroundColor: null,
+                        plotBackgroundImage: null,
+                        plotBorderWidth: 0,
+                        plotShadow: false
+                    },
+
+                    exporting: {
+                        enabled: false
+                    },
+
+                    title: {
+                        text: "<h4 class='text-center " + sensorNameClass + "'>" + value.sensor.name + "</h4>",
+                        margin: 0,
+                        widthAdjust: 0,
+                        useHTML: true
+                    },
+
+                    subtitle: {
+                        text: "<div class='caption'>" + value.timestamp + "</div>",
+                        useHTML: true
+                    },
+
+                    pane: {
+                        startAngle: -150,
+                        endAngle: 150,
+                        background: [{
+                            backgroundColor: {
+                                linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
+                                stops: [
+                                    [0, '#FFF'],
+                                    [1, '#333']
+                                ]
+                            },
+                            borderWidth: 0,
+                            outerRadius: '109%'
+                        }, {
+                            backgroundColor: {
+                                linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
+                                stops: [
+                                    [0, '#333'],
+                                    [1, '#FFF']
+                                ]
+                            },
+                            borderWidth: 1,
+                            outerRadius: '107%'
+                        }, {
+                            // default background
+                        }, {
+                            backgroundColor: '#DDD',
+                            borderWidth: 0,
+                            outerRadius: '105%',
+                            innerRadius: '103%'
+                        }]
+                    },
+
+                    // the value axis
+                    yAxis: {
+                        min: 0,
+                        max: 200,
+
+                        minorTickInterval: 'auto',
+                        minorTickWidth: 1,
+                        minorTickLength: 10,
+                        minorTickPosition: 'inside',
+                        minorTickColor: '#666',
+
+                        tickPixelInterval: 30,
+                        tickWidth: 2,
+                        tickPosition: 'inside',
+                        tickLength: 10,
+                        tickColor: '#666',
+                        labels: {
+                            step: 2,
+                            rotation: 'auto'
+                        },
+                        title: {
+                            text: value.sensor.type.units,
+                            verticalAlign: 'bottom',
+                        },
+                        plotBands: [{
+                            from: 0,
+                            to: 120,
+                            color: '#55BF3B' // green
+                        }, {
+                            from: 120,
+                            to: 160,
+                            color: '#DDDF0D' // yellow
+                        }, {
+                            from: 160,
+                            to: 200,
+                            color: '#DF5353' // red
+                        }]
+                    },
+
+                    series: [{
+                        name: value.sensor.type.name,
+                        data: [value.data],
+                        tooltip: {
+                            valueSuffix: ' ' + value.sensor.type.units
+                        }
+                    }]
+
+                };
+            });
+        };
+
         function checkStatus() {
             Reports.status()
                 .then(function successCallback(response) {
                     vm.sensorList = [];
-                    _.each(response.data, function (value) {
+                    _.each(response.data, function (value, key, list) {
                         imei = value._id;
                         model = value.model.name;
                         sensor = value.sensor;
@@ -137,6 +275,8 @@ angular.module('statusController', [])
                     vm.sensorListCopy = vm.sensorList;
                     vm.onOpenCloseSelectGroup(false);
                     vm.onOpenCloseSelectStatus(false);
+
+                    drawSensors();
                 }, function errorCallback(response) {
                     console.log('Error: ' + response);
                 });
